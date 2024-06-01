65153d7eda4b4fc947fc28ad3c819cf7545922c5
quiquereds <rojasmacedojoseenrique@gmail.com>
2024-06-01 15:09:49 -0600

    prueba git clase 119/465

--- widgets_app/lib/main.dart
+++ widgets_app/lib/main.dart
@@ -10,6 +10,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowMaterialGrid: false,
       home: Scaffold(
         body: Center(
           child: Text('Hello World!'),
