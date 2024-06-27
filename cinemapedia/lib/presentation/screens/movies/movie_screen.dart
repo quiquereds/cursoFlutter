8784009cf2dd877c97729551c1ac238a7478e988
quiquereds <rojasmacedojoseenrique@gmail.com>
2024-06-26 20:10:54 -0600

    feat: :sparkles: screen de detalles de película actualizado de clase 216/465

--- cinemapedia/lib/presentation/screens/movies/movie_screen.dart
+++ cinemapedia/lib/presentation/screens/movies/movie_screen.dart
@@ -1,6 +1,10 @@
+import 'package:cinemapedia/presentation/providers/movies/movie_info_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class MovieScreen extends StatelessWidget {
+import '../../../domain/entities/movie_entity.dart';
+
+class MovieScreen extends ConsumerStatefulWidget {
   static const String name = 'movie-screen';
 
   // Recibimos el ID de la película como parámetro (NO el objeto Movie)
@@ -9,10 +13,182 @@ class MovieScreen extends StatelessWidget {
   const MovieScreen({super.key, required this.movieId});
 
   @override
+  MovieScreenState createState() => MovieScreenState();
+}
+
+class MovieScreenState extends ConsumerState<MovieScreen> {
+  @override
+  void initState() {
+    super.initState();
+    // Hacemos la petición a través del provider
+    /// Se hace uso de read cuando se quiere conocer el valor del
+    /// provider una sola vez
+    ref.read(movieInfoProvider.notifier).loadMovie(widget.movieId);
+  }
+
+  @override
   Widget build(BuildContext context) {
+    /// Creamos un objeto de la película a través del provider
+    /// Aqui se usa el watch para obtener los nuevos valores si es
+    /// que cambia el estado
+    final Movie? movie = ref.watch(movieInfoProvider)[widget.movieId];
+
+    // Si la película es nula, devolvemos un indicador de carga
+    if (movie == null) {
+      return const Scaffold(
+        body: Center(
+          child: CircularProgressIndicator(),
+        ),
+      );
+    }
+
+    // En cambio, si la película existe, mostramos la pantalla
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Movie $movieId'),
+      body: CustomScrollView(
+        physics: const ClampingScrollPhysics(),
+        slivers: [
+          _CustomSliverAppBar(movie: movie),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (context, index) => _MovieDetails(movie: movie),
+              childCount: 1,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _MovieDetails extends StatelessWidget {
+  final Movie movie;
+
+  const _MovieDetails({required this.movie});
+
+  @override
+  Widget build(BuildContext context) {
+    // Obtenemos medidas y temas del dispositivo
+    final size = MediaQuery.of(context).size;
+    final textStyleTheme = Theme.of(context).textTheme;
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        //* Descripción de la película
+        Padding(
+          padding: const EdgeInsets.all(8),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // Imagen de la pelicula
+              ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Image.network(movie.posterPath, width: size.width * 0.3),
+              ),
+              const SizedBox(width: 10),
+              // Descripción
+              SizedBox(
+                width: (size.width - 40) * 0.7,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(movie.title, style: textStyleTheme.titleLarge),
+                    Text(movie.overview),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+        //* Géneros de la película
+        Padding(
+          padding: const EdgeInsets.all(8),
+          child: Wrap(
+            children: [
+              // Barremos todos los generos de la película para hacerlos widget
+              ...movie.genreIds.map(
+                (genre) => Container(
+                  margin: const EdgeInsets.only(right: 10),
+                  child: Chip(
+                    label: Text(genre),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+
+        // TODO: Actores de la película
+        const SizedBox(height: 500)
+      ],
+    );
+  }
+}
+
+class _CustomSliverAppBar extends StatelessWidget {
+  final Movie movie;
+
+  const _CustomSliverAppBar({required this.movie});
+
+  @override
+  Widget build(BuildContext context) {
+    // Obtenemos las dimesiones del dispositivo
+    final size = MediaQuery.of(context).size;
+
+    return SliverAppBar(
+      backgroundColor: Colors.black,
+      // Definimos la altura ocupando el 70% del dispositivo
+      expandedHeight: size.height * 0.4,
+      foregroundColor: Colors.white,
+      flexibleSpace: FlexibleSpaceBar(
+        titlePadding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+        title: Text(
+          movie.title,
+          textAlign: TextAlign.start,
+          style: const TextStyle(
+            fontSize: 20,
+            color: Colors.white,
+          ),
+        ),
+        background: Stack(
+          children: [
+            // Imagen
+            SizedBox.expand(
+              child: Image.network(
+                movie.backdropPath,
+                fit: BoxFit.cover,
+              ),
+            ),
+            // Gradiente de imagen
+            const SizedBox.expand(
+              child: DecoratedBox(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    stops: [0.7, 1.0],
+                    colors: [Colors.transparent, Colors.black87],
+                  ),
+                ),
+              ),
+            ),
+            // Gradiente de flecha
+            const SizedBox.expand(
+              child: DecoratedBox(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    stops: [0.0, 0.3],
+                    colors: [Colors.black87, Colors.transparent],
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
