64c5649ec9d4558216f5d0f80528d005ffd395a8
quiquereds <rojasmacedojoseenrique@gmail.com>
2024-06-26 20:08:28 -0600

    chore: :lipstick: tema de la aplicación actualizado (fuente personalizada)

--- cinemapedia/lib/config/theme/app_theme.dart
+++ cinemapedia/lib/config/theme/app_theme.dart
@@ -4,5 +4,12 @@ class AppTheme {
   ThemeData getTheme() => ThemeData(
         // Usamos un color hexadecimal
         colorSchemeSeed: const Color(0xFF2062F5),
+        // Definimos las fuentes
+        fontFamily: 'Gilroy',
+        textTheme: const TextTheme(
+          titleLarge: TextStyle(fontWeight: FontWeight.bold),
+          titleMedium: TextStyle(fontWeight: FontWeight.bold),
+          titleSmall: TextStyle(fontWeight: FontWeight.bold),
+        ),
       );
 }
