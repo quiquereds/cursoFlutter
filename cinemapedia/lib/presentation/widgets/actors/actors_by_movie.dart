1a8cb574212ccf5d5efd768deb5b4b367a7a3865
quiquereds <rojasmacedojoseenrique@gmail.com>
2024-07-06 20:21:34 -0600

    refactor: :truck: lista de actores como widget independiente

---
+++ cinemapedia/lib/presentation/widgets/actors/actors_by_movie.dart
@@ -0,0 +1,95 @@
+import 'package:animate_do/animate_do.dart';
+import 'package:cinemapedia/presentation/providers/providers.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class ActorsByMovie extends ConsumerWidget {
+  final String movieId;
+
+  const ActorsByMovie({super.key, required this.movieId});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    // Creamos una referencia al estilo de la app
+    final textStyleTheme = Theme.of(context).textTheme;
+    final colors = Theme.of(context).colorScheme;
+
+    // Creamos una referencia a la lista de actores por película
+    final actorsByMovie = ref.watch(actorsByMovieProvider);
+
+    // Mientras no esté la película, mostramos un indicador de carga
+    if (actorsByMovie[movieId] == null) {
+      return const CircularProgressIndicator();
+    }
+
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: SizedBox(
+        // Control del tamaño del ListView
+        height: 120,
+        child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: actorsByMovie[movieId]!.length,
+          itemBuilder: (context, index) {
+            final actorsList = actorsByMovie[movieId]![index];
+
+            return Row(
+              children: [
+                FadeInRight(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: colors.surfaceDim,
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 0, 20, 0),
+                      child: Row(
+                        children: [
+                          ClipRRect(
+                            borderRadius: const BorderRadius.only(
+                              topLeft: Radius.circular(20),
+                              bottomLeft: Radius.circular(20),
+                            ),
+                            child: Image.network(
+                              actorsList.profilePath,
+                              width: 100,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                          const SizedBox(width: 20),
+                          SizedBox(
+                            // Control de ancho de columnas de texto
+                            width: 100,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  actorsList.name,
+                                  maxLines: 2,
+                                  style: textStyleTheme.titleMedium!.copyWith(
+                                      overflow: TextOverflow.ellipsis),
+                                ),
+                                Text(
+                                  actorsList.character!,
+                                  style: textStyleTheme.bodyMedium!.copyWith(
+                                      overflow: TextOverflow.ellipsis),
+                                  maxLines: 2,
+                                ),
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 20),
+              ],
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
--- cinemapedia/lib/presentation/widgets/widgets.dart
+++ cinemapedia/lib/presentation/widgets/widgets.dart
@@ -1,10 +1,12 @@
 // Archivo de barril para los widgets
 
+export 'actors/actors_by_movie.dart';
+
 export 'movies/movie_horizontal_listview.dart';
+export 'movies/movie_masonry.dart';
+export 'movies/movie_rating.dart';
 export 'movies/movies_slideshow.dart';
+
 export 'shared/custom_appbar.dart';
 export 'shared/custom_navbar.dart';
 export 'shared/full_screen_loader.dart';
-
-export 'movies/movie_masonry.dart';
-export 'movies/movie_rating.dart';
